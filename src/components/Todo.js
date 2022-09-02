8ee2909ea671e22086280bdae92221d091f8c880
Mehadi Hasan <themehadihasan@gmail.com>
2022-09-02 18:04:32 +0530

    Initial Commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import todo from '../images/todo-512.webp';
 import './Todo.css';
 
+ // get data from Local Storage 
+
+ const getLocalItemsData = () => {
+    let list = localStorage.getItem('list');
+
+    if(list) {
+        return JSON.parse(localStorage.getItem('list'));
+    } else {
+        return []
+    }
+}
+
 function Todo() {
 
     const [inputData, setInputData] = useState('');
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState(getLocalItemsData());
 
     const addItem = () => {
         if(!inputData) {
@@ -24,13 +36,19 @@ function Todo() {
         setItems(updatedItems);
     }
 
+    // set data to Local Storage
+
+    useEffect(() => {
+        localStorage.setItem('list', JSON.stringify(items)) 
+    }, [items]);
+
     return (
         <>
         <div className='main'>
             <div className='todo'>
                 <figure>
                     <img src={todo} alt='todo-image' />
-                    <h2>Create Today's List</h2>
+                    <h2>To do list...</h2>
                 </figure>
                 <div className='addItems'>
                     <input type="text" value={inputData} onChange={(e) => setInputData(e.target.value)} placeholder='Add item...' />
@@ -43,8 +61,8 @@ function Todo() {
 
                         return (
                             <div className='eachItem' key={ind}>
-                                <p>{elem}</p>
-                                <i class='bx bx-trash' title='Remove item' onClick={() => deleteItem(ind)}></i> 
+                                <p>{elem}<i class='bx bxs-edit-alt'></i></p>
+                                <i class='bx bx-trash' title='Remove item' onClick={() => deleteItem(ind)}></i>
                             </div>
                         )
                     })
